1f2320de398ddeb7259b0ff8553ccd7636d3d6fa
minsoo kim <zlemzlem5656@naver.com>
2021-10-28 13:06:14 +0900

    [test] testfile 추가

---
+++ test/testPackage/MyClassTest.java
@@ -0,0 +1,17 @@
+package testPackage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MyClassTest {
+    int num = 0;
+
+    @Test
+    @DisplayName("Add Test")
+    public void addition() {
+        assertEquals(2, num - 1);
+    }
+
+}
